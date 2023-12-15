38eafa6b39e163f6df03d851f7cfd3980766d588
SYCsasha <124485434+SYCsasha@users.noreply.github.com>
2023-12-16 00:04:09 +0800

    Update 1.clone.sh

--- preset-openwrt/1.clone.sh
+++ preset-openwrt/1.clone.sh
@@ -20,6 +20,7 @@ mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/yichya/luci-app-xray.git
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
 svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
